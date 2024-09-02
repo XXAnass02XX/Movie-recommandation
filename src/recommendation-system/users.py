d8677d187e5b9006c37592acb804cda1b238d595
haydara <anass.haydar@grenoble-inp.org>
2024-09-02 21:13:25 +0100

    now we can get the user profile

--- src/recommendation-system/users.py
+++ src/recommendation-system/users.py
@@ -4,7 +4,12 @@ class Person:
     def __init__(self, full_name):
         self.full_name = full_name
         self.watched_movies = None
+        self.user_profile = None
         #self.Watched_movies = deque() # add elements using .happend()
+
+    def calculate_user_profile(self):
+        self.watched_movies.iloc[:,1 :-1] = self.watched_movies.iloc[:,1 :-1].multiply(self.watched_movies.iloc[:, -1], axis=0)
+        self.user_profile = self.watched_movies.iloc[:, 1:-1].sum().to_frame().T
 
     def __str__(self):
         return f"{self.full_name} "
